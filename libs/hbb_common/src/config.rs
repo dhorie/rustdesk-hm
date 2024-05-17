315cf8abbdc3b4de3e6ddd6a053e4268ee2e8ce5
dhorie <35904524+dhorie@users.noreply.github.com>
2024-05-17 12:33:07 -0300

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -1936,7 +1936,9 @@ pub fn is_incoming_only() -> bool {
         .read()
         .unwrap()
         .get("conn-type")
-        .map_or(false, |x| x == ("incoming"))
+        .map_or(true, |x| x == ("incoming"))
+        //.map_or(false, |x| x == ("incoming"))
+    
 }
 
 #[inline]
@@ -1964,7 +1966,8 @@ pub fn is_disable_tcp_listen() -> bool {
 
 #[inline]
 pub fn is_disable_settings() -> bool {
-    is_some_hard_opton("disable-settings")
+    //is_some_hard_opton("disable-settings")
+    true
 }
 
 #[inline]
